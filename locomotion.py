4d452f5353c54ffb1ed1e5ff91403c2a12856cd1
simon gao <little51@126.com>
2025-11-18 16:47:47 +0800

    重构代码

--- locomotion.py
+++ locomotion.py
@@ -1,43 +1,24 @@
-import distutils.util
 import os
-import subprocess
 import cv2
 
 os.environ['MUJOCO_GL'] = 'egl' 
 
 import json
-import itertools
-import time
-from typing import Callable, List, NamedTuple, Optional, Union
 import numpy as np
 import mediapy as media
 import matplotlib.pyplot as plt
 from datetime import datetime
 import functools
-from typing import Any, Dict, Sequence, Tuple, Union
-from brax import base
-from brax import envs
-from brax import math
-from brax.base import Base, Motion, Transform
-from brax.base import State as PipelineState
-from brax.envs.base import Env, PipelineEnv, State
-from brax.io import html, mjcf, model
-from brax.mjx.base import State as MjxState
 from brax.training.agents.ppo import networks as ppo_networks
 from brax.training.agents.ppo import train as ppo
-from brax.training.agents.sac import networks as sac_networks
-from brax.training.agents.sac import train as sac
 from etils import epath
-from flax import struct
 from flax.training import orbax_utils
-from IPython.display import HTML, clear_output
+from IPython.display import clear_output
 import jax
 from jax import numpy as jp
 from matplotlib import pyplot as plt
 import mediapy as media
-from ml_collections import config_dict
 import mujoco
-from mujoco import mjx
 import numpy as np
 from orbax import checkpoint as ocp
 from mujoco_playground import wrapper
